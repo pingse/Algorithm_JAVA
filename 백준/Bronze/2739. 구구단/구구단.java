434d5ce1f9cd1d15fa7f937524e6c448ff48e198
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2024-09-04 12:56:27 +0900

    [Bronze V] Title: 구구단, Time: 192 ms, Memory: 18496 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
+        int n = sc.nextInt();
 
-        for(int i = 1; i<10; i++){
-           System.out.println(a+" * "+ i +" = "+ (a * i));
+        for (int i = 1; i < 10; i++) {
+            System.out.println(n + " * " + i + " = " + n * i);
         }
     }
 }
