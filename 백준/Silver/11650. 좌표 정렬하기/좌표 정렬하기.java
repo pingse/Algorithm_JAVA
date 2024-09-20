e1cd9a9e3309a18b2bdaa2d11dffef20b7e7bca9
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2024-09-20 15:27:35 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 644 ms, Memory: 50176 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        StringTokenizer stringTokenizer;
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int[][] matrix = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+            matrix[i][0] = Integer.parseInt(stringTokenizer.nextToken());
+            matrix[i][1] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+        bufferedReader.close();
+
+        Arrays.sort(matrix, ((o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return Integer.compare(o1[1], o2[1]);
+            } else {
+                return Integer.compare(o1[0], o2[0]);
+            }
+        }));
+        for (int i = 0; i < n; i++) {
+            bufferedWriter.write(matrix[i][0] + " " + matrix[i][1]+ "\n");
+        }
+        bufferedWriter.flush();
+        bufferedWriter.close();
+
+    }
+}
