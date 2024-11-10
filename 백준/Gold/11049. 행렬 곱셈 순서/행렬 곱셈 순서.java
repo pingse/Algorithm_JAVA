80cdc2d2f794df6b9f0eb238fadfbce12bba6fa0
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2024-11-10 16:26:51 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 220 ms, Memory: 17656 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -4,51 +4,40 @@ import java.util.StringTokenizer;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static Integer[][] dp;
-    static Matrix[] matrix;
+    static int[][] dp;
+    static int[][] matrix;
+
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
-        matrix = new Matrix[n+1];
-        dp = new Integer[matrix.length][matrix.length];
+        matrix = new int[n][2];
+        dp = new int[matrix.length][matrix.length];
 
         StringTokenizer st;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            matrix[i] = new Matrix(Integer.parseInt(st.nextToken()),
-                    Integer.parseInt(st.nextToken()));
+            matrix[i][0] = Integer.parseInt(st.nextToken());
+            matrix[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        bw.write(Integer.toString(calMin(1, n)));
+        for (int i = 0; i < n - 1; i++) {
+            dp[i][i+1] = matrix[i][0] * matrix[i+1][0] * matrix[i+1][1];
+        }
+
+        for (int i = 2; i < n; i++) {
+            for (int j = 0; j + i < n; j++) {
+                int k = j + i;
+
+                dp[j][k] = Integer.MAX_VALUE;
+                for (int z = j; z < k; z++) {
+                    dp[j][k] = Math.min(dp[j][k], dp[j][z] + dp[z + 1][k] + (matrix[j][0] * matrix[z][1] * matrix[k][1]));
+                }
+            }
+
+        }
+
+        bw.write(Integer.toString(dp[0][n-1]));
         bw.flush();
         bw.close();
-    }
-
-    static int calMin(int f, int s) {
-
-        if (f == s) {
-            return 0;
-        }
-
-        if (dp[f][s] == null) {
-            dp[f][s] = Integer.MAX_VALUE;
-            for (int i = f; i < s; i++) {
-                dp[f][s] = Math.min(dp[f][s], matrix[f].row * matrix[i].col * matrix[s].col +
-                        calMin(f, i) +
-                        calMin(i + 1, s));
-            }
-        }
-
-        return dp[f][s];
-    }
-
-    static class Matrix {
-        private int row;
-        private int col;
-
-        Matrix(int a, int b) {
-            this.row = a;
-            this.col = b;
-        }
     }
 
 }
