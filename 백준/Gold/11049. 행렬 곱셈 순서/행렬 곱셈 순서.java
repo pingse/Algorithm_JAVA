fc05a68cefe4db5d3da2df88da63b4b8ec6e7640
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2024-11-12 13:35:17 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 220 ms, Memory: 17616 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -4,13 +4,12 @@ import java.util.StringTokenizer;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int[][] dp;
     static int[][] matrix;
-
+    static int[][] dp;
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
+
         matrix = new int[n][2];
-        dp = new int[matrix.length][matrix.length];
 
         StringTokenizer st;
         for (int i = 0; i < n; i++) {
@@ -19,25 +18,20 @@ public class Main {
             matrix[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < n - 1; i++) {
-            dp[i][i+1] = matrix[i][0] * matrix[i+1][0] * matrix[i+1][1];
+        dp = new int[n + 1][n + 1];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][i+1] = matrix[i-1][0] * matrix[i][0] * matrix[i][1];
         }
-
-        for (int i = 2; i < n; i++) {
-            for (int j = 0; j + i < n; j++) {
-                int k = j + i;
-
-                dp[j][k] = Integer.MAX_VALUE;
-                for (int z = j; z < k; z++) {
-                    dp[j][k] = Math.min(dp[j][k], dp[j][z] + dp[z + 1][k] + (matrix[j][0] * matrix[z][1] * matrix[k][1]));
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j + i <= n; j++) {
+                dp[j][i+j] = Integer.MAX_VALUE;
+                for (int z = j; z < j + i; z++) {
+                    dp[j][i + j] = Math.min(dp[j][i + j], dp[j][z] + dp[z + 1][i + j] + (matrix[j-1][0] * matrix[z-1][1] * matrix[i + j-1][1]));
                 }
             }
-
         }
 
-        bw.write(Integer.toString(dp[0][n-1]));
-        bw.flush();
-        bw.close();
+        System.out.println(dp[1][n]);
     }
-
 }
