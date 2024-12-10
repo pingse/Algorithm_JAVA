2b44b6880fbfe6a587fcfcb75dab9d72a32a9381
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2024-12-10 15:51:51 +0900

    [Gold IV] Title: LCS 2, Time: 152 ms, Memory: 18892 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -1,61 +1,57 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
     static String[] first;
     static String[] second;
-    static int[][] DP;
+    static int[][] dp;
     static String[] text;
     static int next;
     public static void main(String[] args) throws IOException {
         first = br.readLine().split("");
         second = br.readLine().split("");
 
-        DP = new int[first.length+1][second.length+1];
+        dp = new int[first.length+1][second.length+1];
 
         for (int i = 1; i <= first.length; i++) {
             for (int j = 1; j <= second.length; j++) {
                 if (first[i - 1].equals(second[j - 1])) {
-                    DP[i][j] = DP[i - 1][j - 1] + 1;
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    DP[i][j] = Math.max(DP[i][j - 1], DP[i - 1][j]);
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
                 }
             }
         }
-        bw.write(Integer.toString(DP[first.length][second.length]) + "\n");
+        bw.write(Integer.toString(dp[first.length][second.length]) + "\n");
 
-        text = new String[DP[first.length][second.length]];
+        text = new String[dp[first.length][second.length]];
         next = text.length-1;
-
         getText(first.length, second.length);
         for (int i = 0; i < text.length; i++) {
             bw.write(text[i]);
         }
         bw.flush();
         bw.close();
-
+        br.close();
     }
 
-    static void getText(int a, int b) {
-        if (a == 0 || b == 0) {
+    static void getText(int firstLength, int secondLength) {
+        if (firstLength == 0 || secondLength == 0) {
             return;
         }
 
-        if (first[a-1].equals(second[b-1])) {
-            text[next] = first[a-1];
+        if (first[firstLength - 1].equals(second[secondLength - 1])) {
+            text[next] = first[firstLength - 1];
             next--;
-            getText(a - 1, b - 1);
+            getText(firstLength - 1, secondLength - 1);
         } else {
-            if (DP[a][b - 1] > DP[a - 1][b]) {
-                getText(a, b - 1);
+            if (dp[firstLength][secondLength - 1] > dp[firstLength - 1][secondLength]) {
+                getText(firstLength, secondLength - 1);
             } else {
-                getText(a - 1, b);
+                getText(firstLength-1, secondLength);
             }
         }
     }
-
-
-
 }
