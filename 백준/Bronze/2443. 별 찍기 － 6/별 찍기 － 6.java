52c3458368216b9f3627d6c8704723fbf33e6415
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-01-09 12:39:48 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 128 ms, Memory: 16304 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            bw.write(" ".repeat(i) + "*".repeat(2 * (n-i) - 1) + "\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
