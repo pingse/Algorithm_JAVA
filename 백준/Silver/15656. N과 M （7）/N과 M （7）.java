c60d89838ca945f8ac8a04a904bada6ede0df97a
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-01-28 00:43:43 +0900

    [Silver III] Title: N과 M (7), Time: 752 ms, Memory: 299184 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int n, m;
+    static int[] arr;
+    static int[] answer;
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        answer = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        nm(0);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void nm(int depth) throws IOException {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                bw.write(Integer.toString(arr[answer[i]]) + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            answer[depth] = i;
+            nm(depth + 1);
+        }
+    }
+}
