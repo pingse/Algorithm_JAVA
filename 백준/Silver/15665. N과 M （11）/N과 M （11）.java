3bc7874564cf42e2709c2b7cce2b1cadec456b3f
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-01-28 23:28:10 +0900

    [Silver II] Title: N과 M (11), Time: 428 ms, Memory: 123352 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int n, m;
+    static int[] arr;
+    static int[] answer;
+    static boolean[] isUsed;
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        answer = new int[m];
+        isUsed = new boolean[n];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        nm(0);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void nm(int depth) throws IOException {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                bw.write(Integer.toString(answer[i]) + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            if (temp != arr[i]) {
+                answer[depth] = arr[i];
+                temp = answer[depth];
+                nm(depth + 1);
+            }
+        }
+    }
+}
