b50615bc68c85413bbac771ef931bed70a37517e
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-02-05 23:28:53 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 20012 ms, Memory: 149960 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            bw.write(Integer.toString(arr[i]) + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
