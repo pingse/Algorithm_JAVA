f2ec23b73c6f70b132eb195881d6d228c898f3ae
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-02-12 22:14:47 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 152 ms, Memory: 41840 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -12,20 +12,15 @@ public class Main {
         dp[1] = 1L;
         dp[2] = 2L;
         dp[3] = 4L;
+        for (int i = 4; i < 1000001; i++) {
+            dp[i] =( dp[i-1] + dp[i-2] + dp[i-3] ) % 1000000009;
+        }
         for (int i = 0; i < n; i++) {
             int next = Integer.parseInt(br.readLine());
-            bw.write(Long.toString(sum(next)) + "\n");
+            bw.write(Long.toString(dp[next]) + "\n");
         }
         bw.flush();
         bw.close();
         br.close();
-    }
-
-    static long sum(int n) {
-        if (dp[n] == null) {
-            dp[n] = ( sum(n-1) + sum(n-2) + sum(n-3) ) % 1000000009L;
-        }
-
-        return dp[n];
     }
 }
