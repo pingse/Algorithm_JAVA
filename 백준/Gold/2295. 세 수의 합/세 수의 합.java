17e573099611d78c13f103e9481859d1cfed7374
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-02-27 23:16:39 +0900

    [Gold IV] Title: 세 수의 합, Time: 268 ms, Memory: 21280 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -3,31 +3,51 @@ import java.io.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] arr2;
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
         Set<Integer> set = new HashSet<>();
 
         int[] arr = new int[n];
+        arr2 = new int[n * (n+1) / 2];
 
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
         Arrays.sort(arr);
-
+        int count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
-                set.add(arr[i] + arr[j]);
+                arr2[count++] = arr[i] + arr[j];
             }
         }
-        int max = -1;
+        Arrays.sort(arr2);
         for (int i = n - 1; i >= 0; i--) {
             for (int j = 0; j < n; j++) {
-                if (set.contains(arr[i] - arr[j])) {
-                    max = Math.max(max, arr[i]);
+                if (binarySearch(arr[i] - arr[j])) {
+                    System.out.print(arr[i]);
+                    System.exit(0);
                 }
             }
         }
-        System.out.print(max);
+    }
+
+    static boolean binarySearch(int n) {
+        int start = 0;
+        int end = arr2.length-1;
+
+        while (start <= end) {
+            int mid = (start + end) /2;
+
+            if (arr2[mid] > n) {
+                end = mid - 1;
+            } else if (arr2[mid] < n) {
+                start = mid + 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 }
