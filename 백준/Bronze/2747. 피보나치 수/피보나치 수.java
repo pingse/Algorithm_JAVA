c8e56b5030b5726fd02cca03774ebd94d08e8ee4
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-03-20 00:05:32 +0900

    [Bronze II] Title: 피보나치 수, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.print(dp[n]);
+    }
+}
