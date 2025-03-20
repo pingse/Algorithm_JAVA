18c7b0838901cc49a6e3a246a4d0fc8b6519f78d
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-03-20 23:13:23 +0900

    [Silver V] Title: 거스름돈, Time: 104 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -3,36 +3,23 @@ import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int[] dp;
-
     public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
-        dp = new int[n + 1];
-        System.out.print(DP(n));
-    }
-
-    static int DP(int n) {
-        if (n == 5) {
-            return dp[n] = 1;
-        } else if (n == 2) {
-            return dp[n] = 1;
-        }
-        if (n >= 5) {
-            if (dp[n] == 0) {
-                if (DP(n - 5) > 0 && DP(n - 2) > 0) {
-                    dp[n] = Math.min(DP(n - 5) + 1, DP(n - 2) + 1);
-                } else if (DP(n - 5) == -1 && DP(n - 2) == -1) {
-                    dp[n] = -1;
-                } else if (DP(n - 5) > 0 || DP(n - 2) > 0) {
-                    dp[n] = DP(n - 5) == -1 ? DP(n - 2) + 1 : DP(n - 5) + 1;
-                }
-            }
-        } else {
-            if (n % 2 != 0) {
-                return dp[n] = -1;
-            }
-            dp[n] = DP(n-2) + 1;
-        }
-        return dp[n];
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        int temp = 0;
+         while(true){
+             if(N%5 == 0){
+                    count += N/5;
+                 System.out.println(count);
+                 break;
+             }else{
+                 N -= 2;
+                 count++;
+             }
+             if(N < 0){
+                 System.out.println(-1);
+                 break;
+             }
+         }
     }
 }
