265bbecde79ed96597c4228fee503fc96c140cff
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-03-23 22:04:09 +0900

    [Silver V] Title: 최대 상승, Time: 324 ms, Memory: 34208 KB -BaekjoonHub

--- 백준/Silver/25644. 최대 상승/최대 상승.java
+++ 백준/Silver/25644. 최대 상승/최대 상승.java
@@ -6,23 +6,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int[] arr = new int[n + 1];
-        int[][] dp = new int[n + 1][2];
-
-        for (int i = 1; i <= n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        int ans = 0;
+        int min = Integer.MAX_VALUE;
+        while (n-- > 0) {
+            int cur = Integer.parseInt(st.nextToken());
+            ans = Math.max(cur - min, ans);
+            min = Math.min(min, cur);
         }
-        dp[0][0] = Integer.MAX_VALUE;
-        dp[0][1] = 0;
-        for (int i = 1; i <= n; i++) {
-            if (arr[i] < dp[i - 1][0]) {
-                dp[i][0] = arr[i];
-                dp[i][1] = dp[i - 1][1];
-            } else {
-                dp[i][0] = dp[i - 1][0];
-                dp[i][1] = Math.max(arr[i] - dp[i - 1][0], dp[i - 1][1]);
-            }
-        }
-        System.out.print(dp[n][1]);
+        System.out.print(ans);
     }
 }
