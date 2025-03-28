aa45d0811151a47394036f602269108b9316d120
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-03-29 00:19:10 +0900

    [Gold IV] Title: 회사 문화 1, Time: 628 ms, Memory: 77356 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -2,47 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static ArrayList<Integer>[] list;
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        list = new ArrayList[n + 1];
 
-	static List<Integer>[] list;
-	static int[] wv;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		list = new ArrayList[n+1];
-		for(int i=1; i<n+1; i++) {
-			list[i] = new ArrayList<>();
-		}
-		
-		st = new StringTokenizer(br.readLine());
-		for(int a=1; a<n+1; a++) {
-			int b = Integer.parseInt(st.nextToken());
-			if(b!=-1) {
-				list[b].add(a);
-			}
-		}
-		
-		wv = new int[n+1];
-		for(int i=0; i<m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int man = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			wv[man] += w;
-			
-		}
-		dfs(1);
-		for(int i=1; i<n+1; i++) {
-			System.out.print(wv[i]+" ");
-		}
-	}
-	
-	static void dfs(int idx) {
-		for(int nxt : list[idx]) {
-			wv[nxt] += wv[idx];
-			dfs(nxt);
-		}
-	}
+        dp = new int[n + 1];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 1; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
+        for (int i = 1; i <= n; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            if (x == -1) {
+                continue;
+            }
+            list[x].add(i);
+        }
+        for (int i = 1; i <= m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int num = Integer.parseInt(st.nextToken());
+            int count = Integer.parseInt(st.nextToken());
+            dp[num] += count;
+        }
+        dfs(1);
+        for (int i = 1; i <= n; i++) {
+            bw.write(dp[i] + " ");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    static void dfs(int n) {
+        for (int x : list[n]) {
+            dp[x] += dp[n];
+            dfs(x);
+        }
+    }
 }
