13a7c03765e4c07fe94d3e81ce4dacc2d42465ab
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-03-31 23:12:30 +0900

    [Gold IV] Title: 줄세우기, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+        int[] dp = new int[n + 1];
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = 1;
+            for (int j = 1; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(dp[i], max);
+                }
+            }
+        }
+        System.out.print(n - max);
+    }
+}
