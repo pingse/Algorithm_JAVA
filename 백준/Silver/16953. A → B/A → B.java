ebd0e01059b64803a2a66aff8f436b4aef5e5157
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-04-09 23:18:31 +0900

    [Silver II] Title: A → B, Time: 156 ms, Memory: 18668 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -3,33 +3,29 @@ import java.io.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
 
-        boolean[] visited = new boolean[end+1];
-        visited[start] = true;
-        Queue<long[]> queue = new LinkedList<>();
-        queue.add(new long[]{start, 0});
+        Queue<Long> queue = new LinkedList<>();
+        queue.add((long) start);
+        int count = 0;
         while (!queue.isEmpty()) {
-            long[] cur = queue.poll();
+            int size = queue.size();
 
-            if (cur[0] == end) {
-                System.out.print(cur[1] + 1);
-                System.exit(0);
+            for (int i = 0; i < size; i++) {
+                long cur = queue.poll();
+
+                if (cur == end) {
+                    System.out.print(count + 1);
+                    System.exit(0);
+                }
+                if (cur * 2 <= end) queue.add(cur * 2);
+                if (cur * 10 + 1 <= end) queue.add(cur * 10 + 1);
             }
-
-            if (cur[0] * 2 <= end) {
-                queue.add(new long[]{cur[0] * 2, cur[1] + 1});
-                visited[(int) cur[0]*2] = true;
-            }
-
-            if (cur[0] * 10 + 1 <= end) {
-                queue.add(new long[]{cur[0] * 10 + 1, cur[1] + 1});
-                visited[(int) cur[0]*10+1] = true;
-            }
-
+            count++;
         }
 
         System.out.print("-1");
