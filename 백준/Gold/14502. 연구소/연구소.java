4243b619581f78e7a54d184ef57e8be945f1866f
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-05-03 22:59:37 +0900

    [Gold IV] Title: 연구소, Time: 336 ms, Memory: 148588 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -8,48 +8,54 @@ public class Main {
     static int[] dy = {-1, 1, 0, 0};
     static int[] dx = {0, 0, -1, 1};
     static int max = Integer.MIN_VALUE;
+    static List<int[]> list = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         y = Integer.parseInt(st.nextToken());
         x = Integer.parseInt(st.nextToken());
 
         map = new int[y][x];
+        List<int[]> empties = new ArrayList<>();
 
         for (int i = 0; i < y; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             for (int j = 0; j < x; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 2) {
+                    list.add(new int[]{i, j});
+                } else if (map[i][j] == 0) {
+                    empties.add(new int[]{i, j});
+                }
             }
         }
-        dfs(0);
+
+        int E = empties.size();
+        for (int a = 0; a < E - 2; a++) {
+            int[] w1 = empties.get(a);
+            map[w1[0]][w1[1]] = 1;
+
+            for (int b = a + 1; b < E - 1; b++) {
+                int[] w2 = empties.get(b);
+                map[w2[0]][w2[1]] = 1;
+
+                for (int c = b + 1; c < E; c++) {
+                    int[] w3 = empties.get(c);
+                    map[w3[0]][w3[1]] = 1;
+
+                    bfs();
+
+                    map[w3[0]][w3[1]] = 0;
+                }
+                map[w2[0]][w2[1]] = 0;
+            }
+            map[w1[0]][w1[1]] = 0;
+        }
+
         System.out.print(max);
     }
-    static void dfs(int count) {
-        if (count == 3) {
-            bfs();
-            return;
-        }
 
-        for (int i = 0; i < y; i++) {
-            for (int j = 0; j < x; j++) {
-                if (map[i][j] == 0) {
-                    map[i][j] = 1;
-                    dfs(count + 1);
-                    map[i][j] = 0;
-                }
-            }
-        }
-    }
     static void bfs() {
-        Queue<int[]> queue = new LinkedList<>();
-
-        for (int i = 0; i < y; i++) {
-            for (int j = 0; j < x; j++) {
-                if (map[i][j] == 2) {
-                    queue.add(new int[]{i, j});
-                }
-            }
-        }
+        Queue<int[]> queue = new LinkedList<>(list);
 
         int[][] cMap = new int[y][x];
         for (int i = 0; i < y; i++) {
