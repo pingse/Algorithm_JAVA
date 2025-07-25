3cd8e11ee5b8c7e0d9666f0eb28ddc6be82322a8
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-07-25 23:06:21 +0900

    [level 2] Title: 택배상자, Time: 161.85 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -3,45 +3,22 @@ import java.util.*;
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
-        Stack<Integer> container1 = new Stack<>();
-        Stack<Integer> container2 = new Stack<>();
+        Stack<Integer> container = new Stack<>();
         
-        for (int i = order.length; i>=1; i--) {
-            container1.push(i);
-        }
-        
-        while(true) {
-            if (container1.size() == 0 && container2.size() == 0) {
-                break;
-            }
+        for (int i = 1; i<=order.length; i++) {
+            container.push(i);
             
-            if (container1.size() > 0 && container1.peek() == order[answer]) {
-                answer++;
-                container1.pop();
-            } else if (container2.size() > 0 && container2.peek() == order[answer]) {
-                answer++;
-                container2.pop();
-            } else if (container1.size() == 0 && container2.size() > 0){
-                if (container2.peek() == order[answer]) {
+            while(!container.isEmpty()) {
+                if (container.peek() == order[answer]) {
                     answer++;
-                }
-                container2.pop();
-            } else if (container2.size() == 0 && container1.peek() != order[answer]) {
-                container2.push(container1.pop());
-            } else {
-                if (container1.size() > 0 && order[answer] < container1.peek()) {
-                    if (container2.peek() == order[answer]) {
-                        answer++;
-                        container2.pop();
-                    } else {
-                        break;
-                    }
+                    container.pop();
                 } else {
-                    container2.push(container1.pop());
+                    break;
                 }
             }
         }
         
+       
         return answer;
     }
 }
