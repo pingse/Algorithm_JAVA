14701d54edae31f743ac3ea30daf1d5629974da4
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-08-28 23:47:32 +0900

    [level 1] Title: 카드 뭉치, Time: 0.19 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "";
+        
+        Queue<String> queue1 = new LinkedList<>(Arrays.asList(cards1));
+        Queue<String> queue2 = new LinkedList<>(Arrays.asList(cards2));
+        
+        for(int i = 0; i<goal.length; i++) {
+            if (!queue1.isEmpty() && goal[i].equals(queue1.peek())) {
+                queue1.poll();
+            } else if(!queue2.isEmpty() && goal[i].equals(queue2.peek())) {
+                queue2.poll();
+            }  else {
+                answer = "No";
+            }
+        }
+        
+        if (!answer.equals("No")) {
+            answer = "Yes";
+        }
+        
+        return answer;
+    }
+}
