84f1827a63aabe8bd2e56abbc72958df682d9b60
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-09-08 22:44:47 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,34 +1,7 @@
 import java.util.*;
 
 class Solution {
-    static Stack<Integer> stack = new Stack<>();
     public int solution(int n) {
-        ternary(n);
-        int answer = decimal();
-        
-        return answer;
-    }
-    
-    static void ternary(int n) {
-        while(true) {
-            if (n / 3 == 0) {
-                stack.add(n % 3);
-                return;
-            } else {
-                stack.add(n%3);
-                n /= 3;
-            }
-        }
-    }
-    
-    static int decimal() {
-        int size = stack.size();
-        int ans = 0;
-        for (int i = 0; i<size; i++) {
-            int next = (int) Math.pow(3, i);
-            ans += stack.pop() * next;
-        }
-        
-        return ans;
+        return Integer.parseInt(new StringBuilder(Integer.toString(n,3)).reverse().toString(), 3);
     }
 }
