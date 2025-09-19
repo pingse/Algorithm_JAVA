e3ef76c6539b3b2cc5712c779b7750d1f00e9492
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-09-19 21:46:11 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -5,19 +5,8 @@ class Solution
         int answer = 0;
 
         while(a != b) {
-            if (a % 2 == 1) {
-                a /= 2;
-                a += 1;
-            } else {
-                a /= 2;
-            }
-            
-            if (b % 2 == 1) {
-                b /= 2;
-                b += 1;
-            } else {
-                b /= 2;
-            }
+            a = a/2 + a%2;
+            b = b/2 + b%2;
             answer++;
         }
 
