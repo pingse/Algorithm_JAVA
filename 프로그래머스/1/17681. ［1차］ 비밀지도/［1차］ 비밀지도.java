7865147b380a5d67e72a2b17e95aabd2415a3ed2
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-09-22 22:58:53 +0900

    [level 1] Title: [1차] 비밀지도, Time: 10.43 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -3,15 +3,11 @@ import java.util.*;
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[arr1.length];
-        int max = 0;
-        String b = " ";
         for (int i = 0; i<arr1.length; i++) {
             String convert = Integer.toBinaryString(arr1[i] | arr2[i]);
+            convert = String.format("%" + n + "s", convert);
             convert = convert.replace('1', '#');
             convert = convert.replace('0', ' ');
-            if (convert.length() < n) {
-                convert = b.repeat(n - convert.length()) + convert;
-            }
             answer[i] = convert;
         }
         return answer;
