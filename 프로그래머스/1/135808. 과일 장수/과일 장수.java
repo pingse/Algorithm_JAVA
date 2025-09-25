500a6339d0fb62ca86b481848c1aab3c397d6f54
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-09-25 22:27:19 +0900

    [level 1] Title: 과일 장수, Time: 117.31 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -4,10 +4,8 @@ class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
         Arrays.sort(score);
-        for (int i = score.length-1; i>=0; i-=m) {
-            if (i - (m-1) >= 0) {
-                answer += (score[i-(m-1)] * m);
-            }
+        for (int i = score.length; i>=m; i-=m) {
+            answer += (score[i-m] * m);
         }
         return answer;
     }
