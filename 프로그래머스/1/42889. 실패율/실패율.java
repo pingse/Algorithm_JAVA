32ea6b857c9f2f14d69c36f1db504c87122de6fe
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-09-30 23:04:37 +0900

    [level 1] Title: 실패율, Time: 13.37 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -3,23 +3,21 @@ import java.util.*;
 class Solution {
     public int[] solution(int N, int[] stages) {
         Map<Integer, Double> map = new HashMap<>();
+        int[] current = new int[N+2];
+        int arrLength = stages.length;
+        int totalUser = arrLength;
         
-        int[] pass = new int[N+1];
-        int[] current = new int[N+1];
-        
-        for (int i =0; i<stages.length; i++) {
-            for (int j = 0; j<stages[i]; j++) {
-                pass[j] += 1;
-            }
-            current[stages[i]-1] += 1;
+        for (int a : stages) {
+            current[a] += 1;
         }
         
-        for (int i = 0; i<N; i++) {
-            if (current[i] == 0 || pass[i] == 0) {
-                map.put(i+1, 0.0);
+        for (int i = 1; i<= N; i++) {
+            if (totalUser == 0) {
+                map.put(i, 0.0);
             } else {
-                map.put(i+1, (double) current[i]/(double) pass[i]);
+                map.put(i, (double) current[i] / totalUser);
             }
+            totalUser -= current[i];
         }
         
         List<Integer> list = new ArrayList<>(map.keySet());
