d9b45c84242fc0575996991ed37bf7eb3e27d9a8
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-11-20 23:29:44 +0900

    [level 0] Title: 외계어 사전, Time: 0.24 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -3,20 +3,21 @@ import java.util.*;
 class Solution {
     public int solution(String[] spell, String[] dic) {
         int answer = 2;
-        String check = "";
-        for (int i = 0; i<spell.length; i++) {
-            check += String.valueOf(i);
-            for (int j = 0; j<dic.length; j++) {
-                String temp = dic[j];
-                dic[j] = dic[j].replace(spell[i], "");
-                if (!temp.equals(dic[j])) {
-                    dic[j] += String.valueOf(i);
+        
+        for (int i = 0; i<dic.length; i++) {
+            int count = 0;
+            
+            for (int j = 0; j<spell.length; j++) {
+                if (dic[i].contains(spell[j])) {
+                    count++;
                 }
             }
+            if (count == spell.length) {
+                answer = 1;
+                break;
+            }
         }
-        for (String word : dic) {
-            if (word.equals(check)) answer = 1;
-        }
+        
         return answer;
     }
 }
