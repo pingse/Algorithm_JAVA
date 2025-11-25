85c5dd82b81465994bc872b2e0352c0b04ac4403
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-11-25 23:27:21 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 1.40 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,33 +1,5 @@
-import java.util.*;
-
 class Solution {
     public String[] solution(String my_string) {
-        List<String> list = new ArrayList<>();
-        boolean isEmpty = true;
-        int start = 0;
-        int end = 0;
-        for (int i = 0; i<my_string.length(); i++) {
-            if (my_string.charAt(i) == ' ') {
-                if (isEmpty) {
-                    continue;
-                } else {
-                    isEmpty = true;
-                    end = i;
-                    list.add(my_string.substring(start, end));
-                    continue;
-                }
-            } else {
-                if (isEmpty) {
-                    isEmpty = false;
-                    start = i;
-                } else {
-                    continue;
-                }
-            }
-        }
-        if (!isEmpty) {
-            list.add(my_string.substring(start, my_string.length()));
-        }
-        return list.toArray(new String[list.size()]);
+        return my_string.trim().split("\\s+");
     }
 }
