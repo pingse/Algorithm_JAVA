d8c7b7ab314c170d8edab739dce0e37485e014cf
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-12-01 22:23:33 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 2.42 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -8,16 +8,16 @@ class Solution {
         "remain", 3
     );
     
-    public List<int[]> solution(int[][] data, String ext, int val_ext, String sort_by) {
-        List<int[]> list = new ArrayList<>();
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+        List<int[]> answer = new ArrayList<>();
         final int type = INDEX_MAP.get(ext);
         final int sort = INDEX_MAP.get(sort_by);
         
         for (int i = 0; i<data.length; i++) {
-            if (data[i][type] < val_ext) list.add(data[i]);
+            if (data[i][type] < val_ext) answer.add(data[i]);
         }
         
-        Collections.sort(list, (o1, o2) -> o1[sort] - o2[sort]);
-        return list;
+        Collections.sort(answer, (o1, o2) -> o1[sort] - o2[sort]);
+        return answer.toArray(int[][]::new);
     }
 }
