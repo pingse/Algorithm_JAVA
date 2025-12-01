2d558c406f4d71efae6bada92f7cc8261ec33bc4
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2025-12-01 22:29:08 +0900

    [level 1] Title: 소수 만들기, Time: 2.40 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -4,8 +4,8 @@ class Solution {
     public int solution(int[] nums) {
         int answer = 0;
 
-        for (int i = 0; i<nums.length; i++) {
-            for (int j = i+1; j<nums.length; j++) {
+        for (int i = 0; i<nums.length-2; i++) {
+            for (int j = i+1; j<nums.length-1; j++) {
                 for (int k = j+1; k<nums.length; k++) {
                     if (isPrime(nums[i] + nums[j] + nums[k])) answer++;
                 }
