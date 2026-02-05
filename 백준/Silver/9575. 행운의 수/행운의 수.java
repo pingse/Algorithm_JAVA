d7628931624d310cb9aa5966a84d7a084f806359
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2026-02-05 23:42:38 +0900

    [Silver IV] Title: 행운의 수, Time: 180 ms, Memory: 27008 KB -BaekjoonHub

--- 백준/Silver/9575. 행운의 수/행운의 수.java
+++ 백준/Silver/9575. 행운의 수/행운의 수.java
@@ -6,59 +6,42 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        int t = Integer.parseInt(br.readLine());
-
-        Set<Integer> nums = new HashSet<>();
-        Set<Integer> a = new HashSet<>();
-        Set<Integer> b = new HashSet<>();
-        Set<Integer> c = new HashSet<>();
-        Set<Integer> ab = new HashSet<>();
+        int T = Integer.parseInt(br.readLine());
         StringTokenizer st;
 
-        for (int i = 0; i < t; i++) {
-            nums.clear();
-            a.clear();
-            b.clear();
-            c.clear();
-            ab.clear();
-
+        for (int t = 0; t < T; t++) {
             int N = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine(), " ");
-
-            for (int j = 0; j<N; j++) {
-                a.add(Integer.parseInt(st.nextToken()));
-            }
+            int[] A = new int[N];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) A[i] = Integer.parseInt(st.nextToken());
 
             int M = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine(), " ");
-
-            for (int j = 0; j<M; j++) {
-                b.add(Integer.parseInt(st.nextToken()));
-            }
+            int[] B = new int[M];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < M; i++) B[i] = Integer.parseInt(st.nextToken());
 
             int K = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine(), " ");
+            int[] C = new int[K];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < K; i++) C[i] = Integer.parseInt(st.nextToken());
 
-            for (int j = 0; j<K; j++) {
-                c.add(Integer.parseInt(st.nextToken()));
-            }
+            boolean[] seen = new boolean[90001];
+            int ans = 0;
 
-            for (int num1 : a) {
-                for (int num2 : b) {
-                    ab.add(num1+num2);
-                }
-            }
+            for (int a : A)
+                for (int b : B)
+                    for (int c : C) {
+                        int sum = a + b + c;
+                        if (!seen[sum] && isLucky(sum)) {
+                            seen[sum] = true;
+                            ans++;
+                        }
+                    }
 
-            for (int sum1 : ab) {
-                for (int num3 : c) {
-                    int sum = sum1 + num3;
-                    if (isLucky(sum)) nums.add(sum);
-                }
-            }
-
-            System.out.println(nums.size());
+            System.out.println(ans);
         }
     }
+
     static boolean isLucky(int x) {
         if (x == 0) return false;
         while (x > 0) {
