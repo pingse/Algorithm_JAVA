09f5fbb647cd4d74623b7982b0e6e947be893da1
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2026-02-09 22:21:24 +0900

    [Silver III] Title: 특별상이라도 받고 싶어, Time: 476 ms, Memory: 103108 KB -BaekjoonHub

--- 백준/Silver/24460. 특별상이라도 받고 싶어/특별상이라도 받고 싶어.java
+++ 백준/Silver/24460. 특별상이라도 받고 싶어/특별상이라도 받고 싶어.java
@@ -29,22 +29,22 @@ public class Main {
     }
 
     static int findReward(int y, int x, int range) {
-        List<Integer> list = new ArrayList<>();
         if (range == 1) {
-            list.add(chairs[y][x]);
-            list.add(chairs[y][x+1]);
-            list.add(chairs[y+1][x]);
-            list.add(chairs[y+1][x+1]);
-            Collections.sort(list);
-            return list.get(1);
+            return second(chairs[y][x], chairs[y][x+1], chairs[y+1][x], chairs[y+1][x+1]);
         }
 
-        list.add(findReward(y, x, range/2));
-        list.add(findReward(y, x + range, range/2));
-        list.add(findReward(y+range, x, range/2));
-        list.add(findReward(y+range, x+range, range/2));
+        return second(findReward(y, x, range/2),
+                findReward(y, x + range, range/2),
+                findReward(y+range, x, range/2),
+                findReward(y+range, x+range, range/2));
+    }
 
-        Collections.sort(list);
-        return list.get(1);
+    static int second(int a, int b, int c, int d) {
+        int min1 = Math.min(a, b);
+        int max1 = Math.max(a, b);
+        int min2 = Math.min(c, d);
+        int max2 = Math.max(c, d);
+
+        return Math.min(Math.min(max1, max2), Math.max(min1, min2));
     }
 }
