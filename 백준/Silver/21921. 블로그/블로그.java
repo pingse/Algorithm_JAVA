974ea5df58d514751fec49346dfb197f75cdf176
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2026-03-19 22:38:17 +0900

    [Silver III] Title: 블로그, Time: 324 ms, Memory: 35632 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -9,7 +9,6 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int X = Integer.parseInt(st.nextToken());
 
-        Map<Integer, Integer> map = new HashMap<>();
         int[] arr = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i<N; i++) {
@@ -23,14 +22,17 @@ public class Main {
         }
 
         int max = sum;
-
-        map.put(sum, map.getOrDefault(sum, 0) + 1);
+        int count = 1;
 
         for (int i = X; i<N; i++) {
             sum -= arr[i-X];
             sum += arr[i];
-            max = Math.max(max, sum);
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            if (max < sum) {
+                max = sum;
+                count = 1;
+            } else if (max == sum) {
+                count++;
+            }
         }
 
         if (max == 0) {
@@ -39,6 +41,6 @@ public class Main {
         }
 
         System.out.println(max);
-        System.out.println(map.get(max));
+        System.out.println(count);
     }
 }
