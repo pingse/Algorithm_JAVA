9187c666de79885b8fe5bf77f71a440f1655e5f4
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2026-04-23 22:01:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 303.22 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 25.3 MB, 시간: 76.59 ms
+메모리: 96.8 MB, 시간: 303.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 23일 21:57:54
+2026년 04월 23일 22:01:08
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,26 +4,15 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
-        Map<String, Integer> map = new HashMap<>();
+        Arrays.sort(participant);
+        Arrays.sort(completion);
         
-        for (String c : completion) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-        
-        for (String p : participant) {
-            if (map.containsKey(p)) {
-                if (map.get(p) == 0) {
-                    answer = p;
-                    break;
-                } else {
-                    map.put(p, map.getOrDefault(p, 0) - 1);
-                }
-            } else {
-                answer = p;
-                break;
+        for (int i = 0; i<completion.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
             }
         }
         
-        return answer;
+        return participant[participant.length - 1];
     }
 }
