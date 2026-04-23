178a737595c0eafcf8d0224432d02c94fea1cd6a
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2026-04-23 21:57:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.59 ms, Memory: 25.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,21 +1,9 @@
-from collections import defaultdict
-
 def solution(participant, completion):
     answer = ''
-    map = defaultdict(lambda : 0);
-    for name in completion:
-        map[name] += 1;
-        
-    for name in participant:
-        if name in map:
-            if map[name] == 0:
-                answer = name
-            else:
-                map[name] -= 1
-        
-        else:
-            answer = name
-            
-        if answer != '':
-            break
-    return answer
+    participant.sort()
+    completion.sort()
+    
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[len(participant) - 1]
